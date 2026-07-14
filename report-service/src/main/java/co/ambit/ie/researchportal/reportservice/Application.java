20ee0c6119478b0d16b82426aeabde06a9aa8d06
groot <bhaveshlakhpati@gmail.com>
2026-07-14 15:29:38 +0530

    remove individual test gradleTask from modules

--- report-service/src/main/java/co/ambit/ie/researchportal/reportservice/Application.java
+++ report-service/src/main/java/co/ambit/ie/researchportal/reportservice/Application.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ReportServiceApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ReportServiceApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
