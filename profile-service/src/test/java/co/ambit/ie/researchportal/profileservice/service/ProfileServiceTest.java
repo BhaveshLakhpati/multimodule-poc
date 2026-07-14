80980375113e24c8e6c8b3a34077df96d17f1c07
groot <bhaveshlakhpati@gmail.com>
2026-07-14 23:41:15 +0530

    @Test added for ProfileService.fallback()

--- profile-service/src/test/java/co/ambit/ie/researchportal/profileservice/service/ProfileServiceTest.java
+++ profile-service/src/test/java/co/ambit/ie/researchportal/profileservice/service/ProfileServiceTest.java
@@ -2,14 +2,14 @@ package co.ambit.ie.researchportal.profileservice.service;
 
 import co.ambit.ie.researchportal.profileservice.v1.service.ProfileService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class ProfileServiceTest {
-    @MockitoBean
+    @Autowired
     private ProfileService service;
 
     @Test
